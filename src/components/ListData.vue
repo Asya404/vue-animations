ab2f8edc2dea17848fcc42ee65af6841e90e1412
Asya404 <unkiplum@gmail.com>
2023-10-09 15:06:54 +0200

    feat: add animation for transition group

--- src/components/ListData.vue
+++ src/components/ListData.vue
@@ -1,9 +1,9 @@
 <template>
-  <ul>
+  <transition-group tag="ul" name="list-data">
     <li v-for="user in users" :key="user" @click="removeUser(user)">
       {{ user }}
     </li>
-  </ul>
+  </transition-group>
   <div>
     <input type="text" ref="userNameInput" />
     <button @click="addUser">Add User</button>
@@ -39,5 +39,24 @@ li {
   padding: 1rem;
   margin: 10px 0;
   text-align: center;
+}
+.list-data-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.list-data-enter-active {
+  transition: all 1s ease-out;
+}
+.list-data-enter-to,
+.list-data-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+.list-data-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.list-data-leave-active {
+  transition: all 1s ease-in;
 }
 </style>
