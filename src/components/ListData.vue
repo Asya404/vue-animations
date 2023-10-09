1decb4341c9af95dd99863f7ef4a8bb9c28a6662
Asya404 <unkiplum@gmail.com>
2023-10-09 13:12:41 +0200

    feat: add List Data component

---
+++ src/components/ListData.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="user in users" :key="user">{{ user }}</li>
+  </ul>
+  <div>
+    <input type="text" />
+    <button>Add User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: ['Manu', 'Julie', 'Michael'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+li {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
